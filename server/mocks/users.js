42c9885a3b2280c2c3d95d169f1475301342cd98
Andrei Soare <andrei.soare@gmail.com>
2014-12-11 23:08:24 -0800

    Send the search query to the mock server and get proper responses

--- server/mocks/users.js
+++ server/mocks/users.js
@@ -3,14 +3,21 @@ module.exports = function(app) {
   var usersRouter = express.Router();
 
   usersRouter.get('/', function(req, res) {
-    res.send({
-      "users": [
-        {id: "u1", name: "Andrei Soare", "avatar": "img/avatars/avatar-red.png"},
-        {id: "u2", name: "Vlad Berteanu", "avatar": "img/avatars/avatar-blue.png"},
-      ]
-    });
-  });
+    var query = (req.query.q || '').toLowerCase();
 
+    var users = [
+      {id: "u1", name: "Andrei Soare", "avatar": "img/avatars/avatar-red.png"},
+      {id: "u2", name: "Vlad Berteanu", "avatar": "img/avatars/avatar-blue.png"},
+    ];
+
+    if (query) {
+      users = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(query) !== -1;
+      });
+    }
+
+    res.send({users: users});
+  });
   usersRouter.post('/', function(req, res) {
     res.status(201).end();
   });

2c203c5067a360621feaa29a59d7eec37e30e3da
Andrei Soare <andrei.soare@gmail.com>
2014-12-12 14:53:43 -0800

    Add infinite scroll

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -7,9 +7,15 @@ export default Ember.Controller.extend({
   searchText: '',
   searchTextBinding: Ember.Binding.oneWay('q'),
 
+  hasMore: true,
+
   actions: {
     search: function() {
       this.set('q', this.get('searchText'));
+    },
+
+    fetchMore: function(callback) {
+      console.log('Fetch more data!');
     }
   }
 });

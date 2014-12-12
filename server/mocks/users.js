524a93b4774fab21db377e223aca2d3991a08d1d
Andrei Soare <andrei.soare@gmail.com>
2014-12-11 18:15:33 -0800

    Create a model, a RESTAdapter and an http mock server

---
+++ server/mocks/users.js
@@ -0,0 +1,39 @@
+module.exports = function(app) {
+  var express = require('express');
+  var usersRouter = express.Router();
+
+  usersRouter.get('/', function(req, res) {
+    res.send({
+      "users": [
+        {id: "u1", name: "Andrei Soare", "avatar": "img/avatars/avatar-red.png"},
+        {id: "u2", name: "Vlad Berteanu", "avatar": "img/avatars/avatar-blue.png"},
+      ]
+    });
+  });
+
+  usersRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  usersRouter.get('/:id', function(req, res) {
+    res.send({
+      "users": {
+        "id": req.params.id
+      }
+    });
+  });
+
+  usersRouter.put('/:id', function(req, res) {
+    res.send({
+      "users": {
+        "id": req.params.id
+      }
+    });
+  });
+
+  usersRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/users', usersRouter);
+};
